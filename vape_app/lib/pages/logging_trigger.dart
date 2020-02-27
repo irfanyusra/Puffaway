23a7cd1fc67938484350ef1679aa7a77aa114e01
Yusra Irfan <yusrairfan99@hotmail.com>
2020-02-27 14:01:45 -0500

    trigger page base functionality done

--- vape_app/lib/pages/logging_trigger.dart
+++ vape_app/lib/pages/logging_trigger.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoggingTrigger extends StatefulWidget {
   @override
@@ -8,6 +9,55 @@ class LoggingTrigger extends StatefulWidget {
 
 class _LoggingTriggerState extends State<LoggingTrigger> {
   @override
+  List<String> triggers = [
+    'Select one',
+    'trigger1',
+    'trigger2',
+    'trigger3',
+    'trigger4'
+  ];
+
+  List<DropdownMenuItem<String>> dropdownTriggerItems;
+  String selectedTrigger;
+  String thoughts;
+
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final thoughtTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    thoughtTextController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    dropdownTriggerItems = buildDropdownTriggerItems(triggers);
+    selectedTrigger = dropdownTriggerItems[0].value;
+    super.initState();
+  }
+
+  List<DropdownMenuItem<String>> buildDropdownTriggerItems(List triggers) {
+    List<DropdownMenuItem<String>> items = List();
+    for (String t in triggers) {
+      items.add(
+        DropdownMenuItem(
+          value: t,
+          child: Text(t),
+        ),
+      );
+    }
+    return items;
+  }
+
+  onChangeDropdownTriggerItem(String newTrig) {
+    setState(() {
+      selectedTrigger = newTrig;
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -15,43 +65,112 @@ class _LoggingTriggerState extends State<LoggingTrigger> {
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.fromLTRB(5, 10, 2, 0),
-        child: Column(children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            //space evenly
+        padding: const EdgeInsets.fromLTRB(10, 15, 10, 0),
+        child: SingleChildScrollView(
+          child: Column(
             children: <Widget>[
               Container(
                 child: Text(
-                  'r1 c1 ',
+                  'To quit a habit, it is crucial that you practice self awareness. Please help us help you by logging what triggered your session',
                   style: TextStyle(
-                    fontSize: 30,
+                    color: Colors.black,
+                    letterSpacing: 2.0,
+                    fontSize: 18,
                   ),
                 ),
               ),
+              SizedBox(
+                height: 20.0,
+              ),
+              //TODO: Make the trigger drop down rounded rect
+              Container(
+                child: SizedBox(
+                  width: 200,
+                  child: Center(
+                    child: DropdownButton(
+                      value: selectedTrigger,
+                      items: dropdownTriggerItems,
+                      onChanged: onChangeDropdownTriggerItem,
+                    ),
+                  ),
+                ),
+                color: Colors.blue[100],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(child: Text('Selected: $selectedTrigger')),
+              SizedBox(
+                height: 25.0,
+              ),
               Container(
                 child: Text(
-                  'r1c2',
+                  'Another important step is writing down your thoughts. If there is anything special about this session please feel free to log this as well',
                   style: TextStyle(
-                    fontSize: 30,
+                    color: Colors.black,
+                    letterSpacing: 2.0,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+
+              Container(
+                height: 170,
+//                color: Colors.grey[300],
+                padding: EdgeInsets.all(10.0),
+                child: new ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxHeight: 150.0,
+                  ),
+                  child: new Scrollbar(
+                    child: new SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      reverse: true,
+                      child: SizedBox(
+                        height: 150.0,
+                        child: new TextField(
+                          controller: thoughtTextController,
+                          maxLines: 10,
+                          decoration: new InputDecoration(
+                            border: new OutlineInputBorder(
+                              borderRadius: new BorderRadius.circular(25.0),
+                              borderSide: new BorderSide(),
+                            ),
+                            hintText: 'Add your text here',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                child: Text('Next'),
+                onPressed: () {
+                  //TODO: Navigate to the next page and send thought and trigger as variables
+                  print(thoughtTextController.text);
+                  setState(() {
+                    thoughtTextController.text = thoughtTextController.text;
+                    thoughts = thoughtTextController.text;
+                  });
+                },
+              ),
+              Container(
+                child: Text(
+                  '${thoughtTextController.text}',
+                  style: TextStyle(
+                    color: Colors.black,
+                    letterSpacing: 2.0,
+                    fontSize: 18,
                   ),
                 ),
               ),
             ],
           ),
-          Row(
-            children: <Widget>[
-              Container(
-                child: Text(
-                  'r2',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
         ),
       ),
     );
