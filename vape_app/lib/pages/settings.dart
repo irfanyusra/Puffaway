a2569ae51a5fcd4405b8a0b170e49c50cf25b163
Spencer <svecile@uwo.ca>
2020-02-27 23:42:42 -0500

    Create settings.dart

---
+++ vape_app/lib/pages/settings.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  @override
+  settingsState createState() => settingsState();
+}
+
+class settingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar:  AppBar(
+          title: Text('Settings'),
+          centerTitle: true,
+        ),
+
+        body: Center(
+          child:
+        )
+    );
+  }
+}
