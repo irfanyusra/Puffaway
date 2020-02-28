0b1cf5b10e03aac699db0bb7ccdf26faf86351d1
Spencer <svecile@uwo.ca>
2020-02-28 17:55:05 -0500

    Update main.dart

--- vape_app/lib/main.dart
+++ vape_app/lib/main.dart
@@ -1,25 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:vape_app/authenticate/authenticate.dart';
-import 'package:vape_app/pages/all_logs.dart';
 import 'package:vape_app/pages/loading.dart';
-import 'package:vape_app/pages/logging_trigger.dart';
-import 'package:vape_app/pages/recommendation_page.dart';
-import 'package:vape_app/pages/reflections_page.dart';
-import 'package:vape_app/pages/Home.dart';
 
-void main() => runApp(App(
-      /*initialRoute: '/',
-      routes: {
-        '/': (context) => Loading(),
-        '/logging_trigger': (context) => LoggingTrigger(),
-        '/recommendations': (context) => Recommendation(),
-        '/reflections': (context) => Reflections(),
-        '/all_logs': (context) => AllLogs(),
-        //add more routes here
-      },*/
-    )
-//    MyApp()
-);
+void main() => runApp(App());
 
 class App extends StatelessWidget {
   @override
@@ -29,53 +11,3 @@ class App extends StatelessWidget {
     );
   }
 }
-/*class MyApp extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState(){
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    int selectedPage = 0;
-    final List<Widget> pageOptions = [
-      Logging(),
-      LoggingTrigger(),
-      Recommendation()
-    ];
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Main'),),
-        body: pageOptions[selectedPage],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: selectedPage,
-          onTap: (int index) {
-            setState(() {
-              selectedPage = index;
-            });
-          },
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                title: Text('Home')
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.note_add),
-                title: Text('Trigger')
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.assessment),
-                //collections_bookmark, comment, book, import_contacts, note_add, settings
-                title: Text('Recommendation')
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}*/
