80da5a387b081c7b0fcde368d6c14df6630fc054
Spencer <svecile@uwo.ca>
2020-03-14 14:06:18 -0400

    calendar displays how many times you vape each day

--- vape_app/lib/pages/statistics.dart
+++ vape_app/lib/pages/statistics.dart
@@ -1,10 +1,10 @@
-import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter/material.dart';
 import 'package:vape_app/services/database.dart';
 import 'package:vape_app/Models/Log.dart';
 import 'package:vape_app/Models/User.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:vape_app/shared/Calendar.dart';
 import 'package:vape_app/shared/Timer.dart';
 
 class Statistics extends StatefulWidget {
@@ -12,40 +12,13 @@ class Statistics extends StatefulWidget {
   _StatisticsState createState() => _StatisticsState();
 }
 
-Map<String,dynamic> encodeMap(Map<DateTime,dynamic> map) {
-  Map<String,dynamic> newMap = {};
-  map.forEach((key,value) {
-    newMap[key.toString()] = map[key];
-  });
-  return newMap;
-}
-
-Map<DateTime,dynamic> decodeMap(Map<String,dynamic> map) {
-  Map<DateTime,dynamic> newMap = {};
-  map.forEach((key,value) {
-    newMap[DateTime.parse(key)] = map[key];
-  });
-  return newMap;
-}
-
 class _StatisticsState extends State<Statistics> {
-  CalendarController _calendarController;
-  @override
-  void initState() {
-    super.initState();
-    _calendarController = CalendarController();
-  }
 
-  @override
-  void dispose() {
-    _calendarController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
-    Map<DateTime, List<dynamic>> _events;
+
     return StreamProvider<List<Log>>.value(
       value: DatabaseService(uid:user.uid).logs,
       child: Scaffold(
@@ -65,11 +38,12 @@ class _StatisticsState extends State<Statistics> {
                 padding: const EdgeInsets.fromLTRB(0, 30, 0, 30),
                 child: timer(),
               ),
-              TableCalendar( calendarController: _calendarController),
-            ],
+              Calendar(),
+
+              ],
+            ),
           ),
         )
-      ),
     );
   }
 }
---
+++ vape_app/lib/shared/Calendar.dart
@@ -0,0 +1,73 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:vape_app/Models/Log.dart';
+
+class Calendar extends StatefulWidget {
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+
+  CalendarController _calendarController;
+  Map<DateTime, List<dynamic>> _events;
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarController = CalendarController();
+    _events={};
+  }
+
+  Map<String,dynamic> encodeMap(Map<DateTime,dynamic> map) {
+    Map<String,dynamic> newMap = {};
+    map.forEach((key,value) {
+      newMap[key.toString()] = map[key];
+    });
+    return newMap;
+  }
+
+  Map<DateTime,dynamic> decodeMap(Map<String,dynamic> map) {
+    Map<DateTime,dynamic> newMap = {};
+    map.forEach((key,value) {
+      newMap[DateTime.parse(key)] = map[key];
+    });
+    return newMap;
+  }
+  @override
+  void dispose() {
+    _calendarController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final logs = Provider.of<List<Log>>(context);
+
+    setState(() {
+      if(logs!=null) {
+        for (var i = 0; i < logs.length; i++) {
+
+          var date = DateTime.parse(logs[i].dateTime.toDate().toString());
+          var formatter = new DateFormat('yyyy-MM-dd');
+          var formatted = DateTime.parse(formatter.format(date));
+
+          if (_events[formatted] != null) {
+            _events[formatted].add("0");
+          } else {
+            _events[formatted] = ["0"];
+          }
+        }
+      }
+    });
+
+    return TableCalendar(
+        calendarController: _calendarController,
+        events: _events,
+    );
+  }
+}
