c606d7f70b53172c875ad730432b988c3bcb0187
hashimsharkh <hashimabusharkh@gmail.com>
2020-03-16 00:34:18 -0400

    Finsihed backend unit testing

--- vape_app/lib/services/auth.dart
+++ vape_app/lib/services/auth.dart
@@ -3,22 +3,23 @@ import 'package:vape_app/Models/User.dart';
 import 'package:vape_app/services/database.dart';
 
 class AuthService{
-  //sign in anonymously
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
 
+  //Used for testing purposes
+  AuthService({this.auth});
   User _userFromFirebaseUser(FirebaseUser user){
     return user!=null?User(uid:user.uid):null;
   }
 
   // auth change user stream
   Stream<User> get user {
-    return _auth.onAuthStateChanged
+    return auth.onAuthStateChanged
       .map(_userFromFirebaseUser);
   }
 
   Future signInAnon(String name) async {
     try{
-      AuthResult result = await _auth.signInAnonymously();
+      AuthResult result = await auth.signInAnonymously();
       FirebaseUser user = result.user;
 
       await DatabaseService(uid:user.uid).updateUserData(name);
@@ -33,7 +34,7 @@ class AuthService{
   //sign in
    Future signInWithEmailAndPassword(String email, String password)async{
     try{
-      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      AuthResult result = await auth.signInWithEmailAndPassword(email: email, password: password);
       FirebaseUser user = result.user;
 
       return _userFromFirebaseUser(user);
@@ -46,7 +47,7 @@ class AuthService{
   //Register with email and password
   Future registerWithEmailAndPassword(String email, String password)async{
     try{
-      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      AuthResult result = await auth.createUserWithEmailAndPassword(email: email, password: password);
       FirebaseUser user = result.user;
 
       return _userFromFirebaseUser(user);
@@ -59,7 +60,7 @@ class AuthService{
   //sign out
   Future signOut() async{
     try{
-      return await _auth.signOut();
+      return await auth.signOut();
     }catch(e){
       print(e.toString());
       return null;
