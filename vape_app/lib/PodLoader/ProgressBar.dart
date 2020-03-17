62808be8767b4268b737e6e756f8a66bdda985e0
Abdulaziz Chalya <abdulazizchalya@gmail.com>
2020-03-16 22:38:28 -0400

    Revert "Merge branch 'master' into settings2"
    This reverts commit 0cd34131bb12a06af1009e1e8eca10af45b3f1b6, reversing
    changes made to 4591d69788a665c8cfe0fed00f634146d0e58ba1.

--- vape_app/lib/PodLoader/ProgressBar.dart
+++ vape_app/lib/PodLoader/ProgressBar.dart
@@ -2,24 +2,20 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vape_app/services/pods.dart';
-import 'package:provider/provider.dart';
-import 'package:vape_app/Models/User.dart';
-import 'package:vape_app/Models/pod.dart';
-import 'package:vape_app/shared/loading.dart';
-import 'package:vape_app/services/database.dart';
 import 'liquid_custom_progress_indicator.dart';
 
 class ProgressBar extends StatefulWidget {
+
   @override
   _ProgressBarState createState() => _ProgressBarState();
 }
 
 class _ProgressBarState extends State<ProgressBar> {
   double _progressValue;
-  var goal = 10;
+  var goal=10;//database var for goal goes here
 
   final _pod = PodService();
-
+  
   @override
   void initState() {
     super.initState();
@@ -45,88 +41,55 @@ class _ProgressBarState extends State<ProgressBar> {
 
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<User>(context);
-    return StreamBuilder<List<Pod>>(
-        stream: DatabaseService(uid: user.uid).pods,
-        builder: (context, snapshot) {
 
-          if (snapshot.hasData) {
-            var startDateSec = snapshot.data.first.dateTime.seconds;
-            var endDateSec = startDateSec + (goal * 24 * 60 * 60);
-            var nowSec = DateTime
-                .now()
-                .millisecondsSinceEpoch / 1000; //time now in sec
-            var totalSec = endDateSec - startDateSec;
-            var totalSecLeft = endDateSec - nowSec;
+    return Container(
+          padding: EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
 
-            if(totalSecLeft / totalSec>0) {
-                _progressValue = totalSecLeft / totalSec;
-            }else{
-                _progressValue = 0;
-            }
-
-            return Container(
-              padding: EdgeInsets.all(16.0),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                  LiquidCustomProgressIndicator(
+              LiquidCustomProgressIndicator(
                   value: _progressValue,
-                  valueColor: AlwaysStoppedAnimation(Colors.amberAccent),
-                  backgroundColor: Colors.grey.withOpacity(0.5),
-                  direction: Axis.vertical,
-                  shapePath: _buildVapePath(),
-                  ),
-                  //Text('${(_progressValue * 100).round()}%'),
-                  Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: FloatingActionButton( //finish pod
+                  valueColor: AlwaysStoppedAnimation(Colors.amberAccent), // Defaults to the current Theme's accentColor.
+                  backgroundColor: Colors.grey.withOpacity(0.5), // Defaults to the current Theme's backgroundColor.
+                  direction: Axis.vertical, // The direction the liquid moves (Axis.vertical = bottom to top, Axis.horizontal = left to right).
+                  shapePath: _buildVapePath(), // A Path object used to draw the shape of the progress indicator.
+                ),
+              //Text('${(_progressValue * 100).round()}%'),
 
-                        onPressed: () async {
-                          _pod.addPodFinishTime();
-                          //if they acheved their goal
-                          setState(() {
-                            if(totalSecLeft / totalSec<=0)
-                              _showDialog();
-                          });
-                        },
-                        tooltip: 'Finish Pod',
-                        child: Icon(Icons.delete_outline),
-                      ),
-                  ),
-                ],
-              )
-            );
-          } else {
-            return Loading();
-          }
-        }
-    );
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: FloatingActionButton( //finish pod
+                  onPressed: () async {
+                    _pod.addPodFinishTime();
+                    setState(() {
+                      _updateProgress();
+                    });
+                  },
+                  tooltip: 'Finish Pod',
+                  child: Icon(Icons.delete_outline),
+                ),
+              ),
+            ],
+          )
+      );
   }
 
-  void _showDialog() {
-    // flutter defined function
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        // return object of type Dialog
-        return AlertDialog(
-          title: new Text("Congratulations!!"),
-          content: new Text("You met your goal of making your pod last at least ${goal} days. Not everyone can quit cold "
-              "turkey and decreasing the amount you vape by making pods last longer is an important first step."
-              "Pleast navigate to the settings page and set a new goal!"),
-          actions: <Widget>[
-            // usually buttons at the bottom of the dialog
-            new FlatButton(
-              child: new Text("Close"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
+  void _updateProgress() {
+    const oneDay = const Duration(seconds: 1);//updated bar every minute
+    new Timer.periodic(oneDay, (Timer t) async {
+      if (this.mounted) {
+        setState(() {
+          _progressValue -= 1.0 / (goal); //every minute a small piece of bar goes away 1440
+
+          if (_progressValue <= 0.0) {
+            t.cancel();
+            _progressValue = 1.0;
+            return;
+          }
+        });
+      }
+    });
   }
 }
 
